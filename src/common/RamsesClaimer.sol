b2e65b79e52dcdb8009edf349463aa52f50d4c59
schroyar <schroyar@protonmail.com>
2024-07-17 08:49:03 -0300

    update contract name

--- src/common/RamsesClaimer.sol
+++ src/common/RamsesClaimer.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.20;
 import {Claimer} from "src/common/Claimer.sol";
 
 contract RamsesClaimer is Claimer {
-    constructor(address keeper, address initialOwner) Claimer("Jones STIP Farm", keeper, initialOwner) {}
+    constructor(address keeper, address initialOwner) Claimer("Ramses Claimer", keeper, initialOwner) {}
 }
