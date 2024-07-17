dd4316ecf824636eae32079fcd23bb6fcf95faf2
schroyar <schroyar@protonmail.com>
2024-07-16 21:35:49 -0300

    fix mockclaimer

--- src/common/MockClaimer.sol
+++ src/common/MockClaimer.sol
@@ -5,5 +5,5 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Claimer} from "src/common/Claimer.sol";
 
 contract MockClaimer is Claimer {
-    constructor(address[] memory supportedAssets, address keeper) Claimer(supportedAssets, "MockFarm", keeper) {}
+    constructor(address[] memory supportedAssets, address keeper, address initialOwner) Claimer(supportedAssets, "MockFarm", keeper, initialOwner) {}
 }
